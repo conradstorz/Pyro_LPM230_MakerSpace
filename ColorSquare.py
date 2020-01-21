5f3661927825da9cbae7cb28f59193e22293034c
Conrad <conradstorz@gmail.com>
2020-01-20 16:34:21 -0800

    added try/except catching nameserver missing

--- ColorSquare.py
+++ ColorSquare.py
@@ -1,6 +1,7 @@
 # Nothing different here, just import the Pyro4 library along with whatever else you need
 
 import os
+import sys
 import Pyro4
 
 from ColorSquare_object import colorSquare
@@ -15,7 +16,11 @@ daemon = Pyro4.Daemon()
 # Get the URI of the daemon
 uri = daemon.register(colorSquare)
 # Find the nameserver on the network
-ns = Pyro4.locateNS()
+try:
+    ns = Pyro4.locateNS()
+except:
+    print('Nameserver not found.')
+    sys.exit()
 # Let the nameserver know what we answer to and where to find us
 ns.register("square", uri)
 # Listen for and handle requests
