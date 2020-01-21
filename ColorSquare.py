4a7ebbcf39d7c38068d62906d1b27bcbbafb4d9d
Conrad Storz <conradstorz@gmail.com>
2020-01-20 20:54:29 -0500

    correcting try/except loop

--- ColorSquare.py
+++ ColorSquare.py
@@ -1,16 +1,17 @@
-# Nothing different here, just import the Pyro4 library along with whatever else you need
+# Nothing different here,
+# just import the Pyro4 library along with whatever else you need
 
 import os
-import sys
+# import sys
 from time import sleep
 import Pyro4
 
 from ColorSquare_object import colorSquare
 
-print( 'DISPLAY' in os.environ )
-print( 'SSH_CONNECTION' in os.environ )
-print( 'SSH_CLIENT' in os.environ )
-print( 'SSH_TTY' in os.environ )
+print("DISPLAY" in os.environ)
+print("SSH_CONNECTION" in os.environ)
+print("SSH_CLIENT" in os.environ)
+print("SSH_TTY" in os.environ)
 
 # Create a daemon -- Pyro's worker class to serve an object
 daemon = Pyro4.Daemon()
@@ -21,14 +22,13 @@ NameServerFound = False
 while not NameServerFound:
     try:
         ns = Pyro4.locateNS()
-    except:
-        print('Nameserver not found.')
-        sleep(5)
-    finally:
         NameServerFound = True
+    except TypeError:
+        print("Nameserver not found.")
+        NameServerFound = False
+        sleep(5)
+
 # Let the nameserver know what we answer to and where to find us
 ns.register("square", uri)
 # Listen for and handle requests
 daemon.requestLoop()
-
-
